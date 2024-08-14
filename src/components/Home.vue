7d20284263769fe18664fce95f041428fee02da5
Purnima Katiyar <purnimakatiyar48@gmail.com>
2024-08-14 10:29:53 +0530

    some changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -31,7 +31,9 @@
         <p>Climate: {{ day.description }}</p>
         <button
           @click="getMoreDetails" 
-          class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-2"
+          class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br 
+          focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg 
+          text-sm px-5 py-2.5 text-center me-2 mb-2 mt-2"
         >
           More Details
         </button>
