781a72154ab0df124e390b5c257aea9cd4252d54
Purnima Katiyar <purnimakatiyar48@gmail.com>
2024-08-16 11:33:07 +0530

    added background

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <router-view />
+  <div class="overflow-auto" :style="addBackground"><router-view /></div>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-}
+  computed: {
+    addBackground() {
+      return {
+        backgroundImage: `url(${require('@/assets/background2.jpg')})`,
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center', 
+        height: '100%', 
+        width: '100%', 
+      };
+    },
+  },
+};
 </script>
 
 <style>
@@ -16,6 +27,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
